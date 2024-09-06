dbef4017547dc35642eaa3ac0ff7a4f9c33fd37c
yangjinyue0605 <yangjinyue0605@dingtalk.com>
2024-09-06 15:56:53 +0800

    提交deploy.sh文件

---
+++ deploy.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+#编译加部署demo站点
+
+#需要配置如下参数
+#项目路径，在Execute_shell 中配置项目路径
+#export PROJ_PATH=这个jenkins任务在部署机器上的路径
+
+#输入你的环境上tomcat的全路径
+#export TOMCAT_APP_PATH=tomcat在部署机器上的路径
+
+### base函数
+killTomcat(){
+  pid = `ps -ef |grep tomcat|grep java|awk '{print $2}'`
+  echo "tomcat ID list :$pid"
+  if [ "$pid" = ""]
+  then
+    echo "no tomcat pid alive"
+  else
+    kill -9 $pid
+}
+cd $PROJ_PATH/demo
+mvn clean install
+
+#停tomcat
+killTomcat
+
+#删除原有工程
+rm -rf $TOMCAT_APP_PATH/webapps/ROOT
+rm -f $TOMCAT_APP_PATH/webapps/ROOT.jar
+rm -f $TOMCAT_APP_PATH/webapps/demo3-0.0.1-SNAPSHOT.jar
+
+#复制新的工程
+cp $PROJ_PATH/demo/target/demo3-0.0.1-SNAPSHOT.jar $TOMCAT_APP_PATH/webapps/
+
+cd $TOMCAT_APP_PATH/webapps/
+mv demo3-0.0.1-SNAPSHOT.jar ROOT.jar
+
+#启动tomcat
+cd $TOMCAT_APP_PATH
+sh bin/startup.sh
+
+
+
